afa0f7a4e0a87a0cb2fc2f55f02e53023f275998
Kohei-Oishi <b1018090@fun.ac.jp>
2020-11-12 12:41:43 +0900

    [fix] sqlファイルを複数にした

---
+++ db/init/1_createdb.sql
@@ -0,0 +1,4 @@
+-- CREATE SCHEMA IF NOT EXISTS `practice` DEFAULT CHARACTER SET utf8 ;
+-- USE `practice` ;
+--
+CREATE DATABASE practice;
---
+++ db/init/2_createdb.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS practice. users (
+    number INT NOT NULL AUTO_INCREMENT,
+    id VARCHAR(32) NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = Innodb;
