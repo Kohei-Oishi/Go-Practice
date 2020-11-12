5d341731c07821bfe227fb5ea7ae07f5d7d16b8c
Kohei-Oishi <b1018090@fun.ac.jp>
2020-11-12 10:40:16 +0900

    [add] my.conf

--- db/init/schema.sql
+++ db/init/schema.sql
@@ -1,9 +1,11 @@
-CREATE SCHEMA IF NOT EXISTS `Go-Practice` DEFAULT CHARACTER SET utf8 ;
-USE `Go-Practice` ;
+CREATE SCHEMA IF NOT EXISTS `practice` DEFAULT CHARACTER SET utf8 ;
+USE `practice` ;
 
-CREATE TABLE IF NOT EXISTS `Go-Practice`. users (
+CREATE TABLE IF NOT EXISTS `practice`. `users` (
     number INT NOT NULL AUTO_INCREMENT,
     id VARCHAR(32) NOT NULL,
     name VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
-);
+)ENGINE = Innodb,
+DEFAULT character set utf8,
+COMMENT = 'ユーザー';
